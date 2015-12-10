7fe947cfe33df3feab04a53935f3ed80ebf9b248
Ajay Dudani <adudani@codeaurora.org>
2015-12-10 12:19:16 -0800

    msm: kgsl: Fix direct references to HZ
    Make the various timeout values HZ agnostic by using the proper
    macros and values instead.
    
    Change-Id: I906b948657c8873518042c7465272c98c5391e59
    Signed-off-by: Suman Tatiraju <sumant@codeaurora.org>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -50,7 +50,7 @@ int graphics_boost = 4;
 #endif
 
 /* Number of jiffies for a full thermal cycle */
-#define TH_HZ			20
+#define TH_HZ			(HZ/5)
 
 #define KGSL_MAX_BUSLEVELS	20
 
@@ -1332,7 +1332,7 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 
 	pwr->power_flags = 0;
 
-	pwr->interval_timeout = pdata->idle_timeout;
+	pwr->interval_timeout = msecs_to_jiffies(pdata->idle_timeout);
 	pwr->strtstp_sleepwake = pdata->strtstp_sleepwake;
 
 	if (kgsl_property_read_u32(device, "qcom,pm-qos-active-latency",
