ba4f3bb160e5cf9ecb0904cc81550d5bddeb7e5d
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-09 02:16:36 +0000

    mm: page_alloc: don't dump pages to fix a compilation error I am unable to fix for some reason
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: RazorSharp <kirananto@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6437,6 +6437,7 @@ static const struct trace_print_flags pageflag_names[] = {
 
 static void dump_page_flags(unsigned long flags)
 {
+#if 0
 	const char *delim = "";
 	unsigned long mask;
 	int i;
@@ -6464,14 +6465,17 @@ static void dump_page_flags(unsigned long flags)
 		printk("%s%#lx", delim, flags);
 
 	printk(")\n");
+#endif
 }
 
 void dump_page(struct page *page)
 {
+#if 0
 	printk(KERN_ALERT
 	       "page:%p count:%d mapcount:%d mapping:%p index:%#lx\n",
 		page, atomic_read(&page->_count), page_mapcount(page),
 		page->mapping, page->index);
 	dump_page_flags(page->flags);
 	mem_cgroup_print_bad_page(page);
+#endif
 }
