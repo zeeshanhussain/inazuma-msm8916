028fc151116b6d1aeb7ff942c20c215a6637a4cc
zeeshanhussain <zeeshanhussain12@gmail.com>
2016-03-24 10:13:26 +0530

    Fix compilation

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -1186,7 +1186,7 @@ static unsigned int fanout_demux_cpu(struct packet_fanout *f,
 	return smp_processor_id() % num;
 }
 
-static unsigned int fanout_demux_rnd(struct packet_fanout *f,
+static inline unsigned int fanout_demux_rnd(struct packet_fanout *f,
 				     struct sk_buff *skb,
 				     unsigned int num)
 {
