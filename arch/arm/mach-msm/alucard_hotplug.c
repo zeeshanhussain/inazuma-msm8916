3428d8920b29d07e5b895c273e1d155836a996df
3ric7arias <ericfari15@gmail.com>
2016-05-15 03:22:23 -0300

    alucard_hotplug: little changes

--- arch/arm/mach-msm/alucard_hotplug.c
+++ arch/arm/mach-msm/alucard_hotplug.c
@@ -64,9 +64,9 @@ static struct hotplug_tuners {
 } hotplug_tuners_ins = {
 	.hotplug_sampling_rate = 50,
 	.hotplug_enable = 0,
-	.min_cpus_online = 8,
+	.min_cpus_online = 2,
 	.maxcoreslimit = NR_CPUS,
-	.maxcoreslimit_sleep = 2,
+	.maxcoreslimit_sleep = 1,
 	.hotplug_suspend = 0,
 	.suspended = false,
 	.force_cpu_up = false,
